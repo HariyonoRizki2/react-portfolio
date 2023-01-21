6a9c5f2222c5be2e4a58a006d806a996938238e5
HARIYONO RIZKI <nabilaputridwianjani526@gmail.com>
2023-01-21 21:33:53 +0700

    Create lang.js

---
+++ public/lang.js
@@ -0,0 +1,63 @@
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "ID"){
+                window.location.href = 'https://portfolio.kiki-id.tk/id/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "US"){
+                window.location.href = 'https://portfolio.kiki-id.tk/en_us/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "UK"){
+                window.location.href = 'https://portfolio.kiki-id.tk/en_uk/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "AU"){
+                window.location.href = 'https://portfolio.kiki-id.tk/en_au/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "JP"){
+                window.location.href = 'https://portfolio.kiki-id.tk/jp/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "KR"){
+                window.location.href = 'https://portfolio.kiki-id.tk/kr/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "FR"){
+                window.location.href = 'https://portfolio.kiki-id.tk/fr/';
+           }
+        });
+    });
+
+fetch('https://ipinfo.io/json').then((res) => {
+        res.json().then((data) => {         
+            if(data.country === "RU"){
+                window.location.href = 'https://portfolio.kiki-id.tk/ru/';
+           }
+        });
+    });
